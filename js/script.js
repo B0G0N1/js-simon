068c26432c87ada3a50d3eeff8f157981aa01833
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-10 01:34:19 +0200

    [FEAT] Add selectDifficulty function

--- js/script.js
+++ js/script.js
@@ -3,31 +3,70 @@ const container = document.getElementById('number-container');
 
 // Il gioco parte con l'evento click del bottone 'play'
 play.addEventListener("click", function () {
-    let randomNumbers = generateArrayNumbers(5);
-    let positionNumebrs = generateArrayNumbers(10);
+    totalNumber = selectDifficulty();
+    let randomNumbers = generateArrayNumbers(totalNumber);
+    let positionNumbers = generatePositionNumbers(totalNumber);
+    console.log(totalNumber);
     console.log(randomNumbers);
-    randomArrayPrint(randomNumbers, positionNumebrs);
+    console.log(positionNumbers);
+    randomArrayPrint(randomNumbers, positionNumbers);
 });
 
 
-// Funzione per generare un Array di numeri casuali
+function selectDifficulty() {
+    let count;
+    do {
+        count = prompt("Quanti numeri vuoi provare a memorizzare?");
+        count = parseInt(count);
+        if (isNaN(count) || count < 1 || count > 10) {
+            alert("Inserisci un numero valido tra 1 e 10");
+        }
+    } while (isNaN(count) || count < 1 || count > 10);
+    return count;
+}
+
+
+
+// Funzione per generare un Array di numeri casuali diversi tra loro
 function generateArrayNumbers(numbers) {
     let array = [];
-    for (let i = 0; i < numbers; i++) {
+    let i = 0
+    while (i < numbers) {
         let number = Math.floor(Math.random() * 100) + 1;
-        array.push(number);
+        if (!array.includes(number)) {
+            array.push(number);
+            i++;
+        }
     }
-    return arrayNumbers;
+    return array;
 }
+
+
+// Funzione per generare ...
+function generatePositionNumbers(numbers) {
+    let array = [];
+    let i = 0
+    while (i < numbers) {
+        let number = Math.floor(Math.random() * 1000) + 1;
+        if (!array.includes(number)) {
+            array.push(number);
+            i++;
+        }
+    }
+    return array;
+}
+
 
 // Funzione per stampare i valori di un Array a schermo, in posizione random
 function randomArrayPrint(array, position) {
-    for (let i = 0; i < array.lenght; i++) {
+    container.innerHTML = '';
+    for (let i = 0; i < array.length; i++) {
         const number = document.createElement('div');
         number.classList.add('number');
         number.innerText = i;
         number.style.top = `${position[i]}px`;
-        number.style.left = `${position[position.lenght-i]}px`;
+        number.style.left = `${position[position.length - i]}px`;
         container.appendChild(number);
+        console.log(number);
     }
 }
