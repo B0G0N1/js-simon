8672f0ce143453bc861b86c0ea3eee887398844f
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-10 09:26:40 +0200

    [FEAT] Fix final issue, add comments, FINAL PROJECT

--- js/script.js
+++ js/script.js
@@ -2,35 +2,35 @@
 const play = document.getElementById("play");
 const container = document.getElementById('number-container');
 
-// Costanti per altezza container per calcolare la posizione random dei numeri all'interno
+
+// Variabili per dimensione container e per calcolare la posizione random dei numeri all'interno
 const range = 50;
 const min = 20;
 const height = container.offsetHeight - (range + min);
 const width = container.offsetWidth - (range + min);
 
-// Countdown
-let time = document.getElementById('seconds');
+
+// Variabili per Countdown
+const time = document.getElementById('seconds');
 let clock;
+
 
 // Il gioco parte con l'evento click del bottone 'play'
 play.addEventListener("click", function () {
-    const totalNumber = selectDifficulty();
-    const randomNumbers = generateArrayNumbers(totalNumber, 100, 1, 1);
+    let totalNumber = selectDifficulty();
+    let randomNumbers = generateArrayNumbers(totalNumber, 100, 1, 1);
     const verticalPositionNumbers = generateArrayNumbers(totalNumber, height, min, range);
     const horizontalPositionNumbers = generateArrayNumbers(totalNumber, width, min, range);
-    console.log(randomNumbers);
-    console.log(verticalPositionNumbers);
-    console.log(horizontalPositionNumbers);
-    console.log(width);
-    console.log(height);
     randomArrayPrint(randomNumbers, verticalPositionNumbers, horizontalPositionNumbers);
     startCountdown();
     setTimeout(() => {
-        insertNumbers(totalNumber);
-    }, 6000);    
+        let userNumbers = insertNumbers(totalNumber);
+        checkNumbers(randomNumbers, userNumbers);
+    }, 31000);
 });
 
-// Funzione per inserire quanti numeri da memorizzare
+
+// Funzione per selezionare quanti numeri da memorizzare
 function selectDifficulty() {
     let count;
     do {
@@ -43,36 +43,33 @@ function selectDifficulty() {
     return count;
 }
 
-// Funzione per generare un Array di numeri casuali diversi tra loro,
-// a cui passo un valore massimo, minimo, quanti valori voglio e un range.
+
+// Funzione per generare un Array di numeri casuali diversi tra loro, a cui passo un massimo, minimo, quanti valori voglio e un range.
 function generateArrayNumbers(value, max, min, range) {
     const array = [];
-    let i = 0;
-    while (i < value) {
+    while (array.length < value) {
         const number = Math.floor(Math.random() * (Math.floor((max - min) / range) + 1)) * range + min;
         if (!array.includes(number)) {
             array.push(number);
-            i++;
         }
     }
     return array;
 }
 
-// Funzione per stampare i valori di un Array a schermo, in posizione random
-// Con la responsività si rompe, ma per adesso va bene così
-function randomArrayPrint(array, yposition, xposition) {
-    container.innerHTML = '';
-    for (let i = 0; i < array.length; i++) {
+
+// Funzione per stampare i valori di un Array a schermo, in posizione random, con la responsività si rompe, ma mi accontento
+function randomArrayPrint(randomNumbers, verticalPositionNumbers, horizontalPositionNumbers) {
+    for (let i = 0; i < randomNumbers.length; i++) {
         const number = document.createElement('div');
         number.classList.add('number');
-        number.innerText = array[i];
-        number.style.bottom = `${yposition[i]}px`;
-        number.style.left = `${xposition[i]}px`;
+        number.innerText = randomNumbers[i];
+        number.style.bottom = `${verticalPositionNumbers[i]}px`;
+        number.style.left = `${horizontalPositionNumbers[i]}px`;
         numberPadding(number);
         container.appendChild(number);
-        console.log(number);
     }
 }
+
 
 // Funzione per sistemare padding/dimensione numeri
 function numberPadding(number) {
@@ -84,17 +81,19 @@ function numberPadding(number) {
     }
 }
 
+
 // Funzione per far partire un countdown di 30 secondi
 function startCountdown() {
-    let seconds = 5;
+    let seconds = 30;
     if (clock) {
         clearInterval(clock);
     }
     time.innerText = `Countdown: ${seconds}`;
     clock = setInterval(function () {
-        if (seconds === 0) {
+        if (seconds === 1) {
             clearInterval(clock);
-            time.innerText = "Tempo scaduto!";
+            time.innerText = '';
+            container.innerHTML = '';
         } else {
             seconds--;
             time.innerText = `Countdown: ${seconds}`;
@@ -111,5 +110,17 @@ function insertNumbers(totalNumber) {
         let input = prompt(`Inserisci il numero ${i + 1}: `);
         userNumber.push(input);
     }
-    console.log(userNumber);
+    return userNumber;
+}
+
+
+// Funzione per controllare quanti numeri ci siamo ricordati
+function checkNumbers(randomNumbers, userNumbers) {
+    let correctNumbers = [];
+    for (let i = 0; i < userNumbers.length; i++) {
+        if (randomNumbers.includes(parseInt(userNumbers[i]))) {
+            correctNumbers.push(parseInt(userNumbers[i]));
+        }
+    }
+    alert("Hai indovinato " + correctNumbers.length + " numeri: " + correctNumbers);
 }
